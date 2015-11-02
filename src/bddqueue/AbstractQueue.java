9873b1ac5ab1b7fbb51eb5e1addb8abf20eed310
Kate <Kate@127.0.0.1>
2015-11-02 14:37:16 -0500

    File /Users/Kate/Documents/workspace/QueueComponent/src/bddqueue/CircArrayQueue.java changed at Mon Nov 02 14:37:15 EST 2015

---
+++ src/bddqueue/AbstractQueue.java
@@ -0,0 +1,5 @@
+package bddqueue;
+
+public class AbstractQueue {
+
+}
---
+++ src/bddqueue/CircArrayQueue.java
@@ -0,0 +1,98 @@
+package bddqueue;	  	
+	  	
+public class CircArrayQueue<E> implements Queue<E> {
+	  	
+	  	
+    private E[] contents;  	
+    private int first;
+    private int length;
+    private int capacity;
+	
+    @SuppressWarnings("unchecked")	  	
+    public CircArrayQueue(int max) {	  	
+        if(max <= 0){  	
+            throw new IllegalArgumentException();  	
+        }
+  	
+        contents = (E[]) new Object[max];  	
+        first = 0; 	
+        length = 0;  	
+        capacity = max; 	
+    }
+	  	
+    public int capacity() {	  	
+        return capacity;	  	
+  	}
+
+    public void clear() {  	
+        length = 0;
+    }
+	  	
+    public E dequeue() throws IllegalStateException {	  	
+        if(length!=0){ 	
+            E x = contents[first];	
+            first++;	  	
+            length--;  	
+            return x;  	
+        } 	
+        else{  	
+            throw new IllegalStateException();
+        }  	
+    }
+	
+    public void enqueue(E element) throws NullPointerException, IllegalStateException {	
+        if(element == null){	  	
+            throw new NullPointerException();	  	
+        }  	
+        else{  	
+            if(length<capacity){  	
+                contents[length] = element;  	
+                length++; 	
+            } 	
+            else{ 	
+                throw new IllegalStateException();	
+            }	  	
+        }	  	
+    }
+	  	
+    public boolean isEmpty() { 	
+        if(length == 0){ 	
+            return true;	
+        } 	
+        else{  	
+        return false;  	
+        }	  	
+    }
+	  	
+    public boolean isFull() {  	
+        if(length == capacity){	
+            return true;	
+        }  	
+        else{ 	
+        return false;  	
+        }  	
+    }
+	  	
+    public int length() {	
+        return length;	  	
+    }
+	  	
+    public E peek() throws IllegalStateException {	
+        if(isEmpty()==true){ 	
+            throw new IllegalStateException();
+    }	
+        return contents[(first+ length -1) % capacity]; 	
+    }
+  	
+    public String toString(){  	
+        String x = "(";	  	
+        for(int i = first; i<length(); i++){	  	
+            x = x + contents[i]; 	
+            if(i<length()-1){  	
+                x = x + ", ";  	
+            }	  	
+        }  	
+        x = x + ")" + capacity;	  	
+        return x;  	
+    }	  	
+}
