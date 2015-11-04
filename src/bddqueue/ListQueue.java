eba7b8924f55453a99dd975e8a83d2ee5b1d09af
Kate <Kate@127.0.0.1>
2015-11-03 23:49:18 -0500

    File /Users/Kate/Documents/workspace/QueueComponent/src/bddqueue/ListQueue.java changed at Tue Nov 03 23:49:18 EST 2015

--- src/bddqueue/ListQueue.java
+++ src/bddqueue/ListQueue.java
@@ -1,5 +1,80 @@
 package bddqueue;
 
-public class ListQueue {
+import java.util.*;
 
+public class ListQueue<E> extends AbstractQueue<E> {
+	
+	private List<E> list;
+    private int first;
+    private int length;
+    private int capacity;
+	
+	public ListQueue(int max) {
+		super(max);
+		list = new ArrayList<>();
+	}
+
+	public int length() {
+		return list.size();
+	}
+
+	public void clear() {
+		list.clear();
+	}
+
+	public Queue<E> newInstance() {
+		return new ListQueue<>(capacity);
+	}
+
+	public boolean isValid() {
+		for(E elem : list) {
+			if(elem == null) 
+				return false;
+		}
+		if(list.size() > capacity) 
+			return false;
+		if(capacity <= 0) 
+			return false;
+		return true;
+	}
+	
+	public Iterator<E> iterator() {
+		return list.iterator();
+	}
+
+	@Override
+	public void enqueue(E element) throws NullPointerException, IllegalStateException {
+		if(element == null){	  	
+            throw new NullPointerException();	  	
+        }  	
+        else{  	
+            if(list.size()<capacity){  	
+            	list.add(list.size(), element);  	
+                length++;
+            } 	
+            else{ 	
+                throw new IllegalStateException();	
+            }	  	
+        }
+	}
+
+	@Override
+	public E dequeue() throws IllegalStateException {
+		if(length!=0){ 	
+            E x = list.get(0);	
+            first++;	  	
+            length--;  	
+            return x;  	
+        } 	
+        else{  	
+            throw new IllegalStateException();
+        }  	
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void append(Queue<E> that) throws NullPointerException, IllegalStateException {
+		that = (Queue<E>) dequeue();
+		enqueue((E) that);
+	}
 }
