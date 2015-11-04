ac738b6f79494b3a7318d3de150e8489e42fa03b
Kate <Kate@127.0.0.1>
2015-11-04 15:34:45 -0500

    File /Users/Kate/Documents/workspace/QueueComponent/Test/bddqueue/CircArrayQueueTest.java changed at Wed Nov 04 15:34:45 EST 2015

--- Test/bddqueue/CircArrayQueueTest.java
+++ Test/bddqueue/CircArrayQueueTest.java
@@ -131,7 +131,7 @@ public class CircArrayQueueTest {
 	     
 	     @Test(expected = NoSuchElementException.class)
 	     public void IteratorTextException(){
-	    	 assertEquals(q3.iterator().hasNext(), 4);
+	    	 assertTrue(q3.iterator().hasNext());
 	     }
 		  	
 	 }
