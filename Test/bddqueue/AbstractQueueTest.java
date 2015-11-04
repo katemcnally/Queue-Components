898dc441f4a972cd4af8df71fc281a505d7de209
Kate <Kate@127.0.0.1>
2015-11-04 16:23:35 -0500

    File /Users/Kate/Documents/workspace/QueueComponent/Test/bddqueue/AbstractQueueTest.java changed at Wed Nov 04 16:23:26 EST 2015

--- Test/bddqueue/AbstractQueueTest.java
+++ Test/bddqueue/AbstractQueueTest.java
@@ -6,14 +6,24 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class AbstractQueueTest {
+	
+	AbstractQueue<Object> q = new CircArrayQueue<>(20);
+	AbstractQueue<Object> q2 = new ListQueue<>(10);
 
 	@Before
 	public void setUp() throws Exception {
+		  q.enqueue("Hello");
+	      q.enqueue("Shaked");	
+	      q.enqueue("Kate");	
+	      q.enqueue("Audrey");	
+	      q2.enqueue(1);	
+	      q2.enqueue(2);  	
+	      q2.enqueue(3);
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void Capacitytest() {
+		assertEquals(q.capacity(), 20);
 	}
 
 }
